3d5e34f1176ade4bc6022c688a4c741b2ae872c0
Riccardo Cogoni <cogonir@live.com>
2021-04-04 15:36:52 +0100

    Add global authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
 function App() {
   return <h1>Hello World!</h1>;
 }
 
-export default App;
+export default withAuthenticator(App);
