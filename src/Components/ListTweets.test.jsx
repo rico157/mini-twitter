fe989a21860e0808ad54b79ff19d354739a854a9
Riccardo Cogoni <cogonir@live.com>
2021-04-05 00:30:58 +0100

    Add test - multiple tweets are in the document

--- src/Components/ListTweets.test.jsx
+++ src/Components/ListTweets.test.jsx
@@ -13,3 +13,30 @@ test("renders a tweet", () => {
   const { getByText } = render(<ListTweets tweets={listTweets} />);
   expect(getByText("My first mini-tweet!")).toBeInTheDocument();
 });
+
+test("renders multiple tweets", () => {
+  const listTweets = [
+    {
+      createdAt: "2021-04-04T19:34:54.663Z",
+      id: "5d99e337-bff2-445f-a5fd-6a668c05e953",
+      likes: null,
+      message: "My third mini-tweet!"
+    },
+    {
+      createdAt: "2021-04-04T19:34:54.663Z",
+      id: "5d99e337-bff2-445f-a5fd-6a668c05e952",
+      likes: null,
+      message: "My second mini-tweet!"
+    },
+    {
+      createdAt: "2021-04-04T19:34:54.663Z",
+      id: "5d99e337-bff2-445f-a5fd-6a668c05e951",
+      likes: null,
+      message: "My first mini-tweet!"
+    }
+  ];
+  const { getByText } = render(<ListTweets tweets={listTweets} />);
+  expect(getByText("My first mini-tweet!")).toBeInTheDocument();
+  expect(getByText("My second mini-tweet!")).toBeInTheDocument();
+  expect(getByText("My third mini-tweet!")).toBeInTheDocument();
+});
