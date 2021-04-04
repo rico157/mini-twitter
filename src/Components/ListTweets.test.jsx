0c97f1ab3bbb785d088db0bbfcf171e5192642ce
Riccardo Cogoni <cogonir@live.com>
2021-04-05 00:30:28 +0100

    Add test - tweet is in the document

---
+++ src/Components/ListTweets.test.jsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import ListTweets from "./ListTweets";
+
+test("renders a tweet", () => {
+  const listTweets = [
+    {
+      createdAt: "2021-04-04T19:34:54.663Z",
+      id: "5d99e337-bff2-445f-a5fd-6a668c05e951",
+      likes: null,
+      message: "My first mini-tweet!"
+    }
+  ];
+  const { getByText } = render(<ListTweets tweets={listTweets} />);
+  expect(getByText("My first mini-tweet!")).toBeInTheDocument();
+});
