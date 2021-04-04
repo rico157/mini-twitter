25e703b28c2170083a964e9579a01877f3c3e4c4
Riccardo Cogoni <cogonir@live.com>
2021-04-04 21:16:42 +0100

    Add tweets data to the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import awsconfig from "./aws-exports";
+import { getTweets } from "./utils/api";
 
 Amplify.configure(awsconfig);
 
 export function App() {
   const [tweets, setTweets] = useState([]);
 
+  useEffect(() => {
+    getTweets().then((tweetsData) => setTweets(tweetsData));
+  }, []);
+
   return (
     <>
       <h1>Hello World!</h1>
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <h2>{tweet.message}</h2>
+          <h2>{tweet.id}</h2>
+          <h2>{tweet.createdAt}</h2>
+          <h2>{tweet.likes}</h2>
+        </div>
+      ))}
       <AmplifySignOut />
     </>
   );
