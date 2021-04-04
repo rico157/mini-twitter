c0fc4207d157e701714f2543158937fb8c74aa37
Riccardo Cogoni <cogonir@live.com>
2021-04-04 19:28:13 +0100

    Add Sign-Out button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
-function App() {
-  return <h1>Hello World!</h1>;
+export function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <AmplifySignOut />
+    </>
+  );
 }
 
 export default withAuthenticator(App);
