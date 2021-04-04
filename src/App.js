85f6d1493ef5d8ef3cea5e4250535ccfde5b36d7
Riccardo Cogoni <cogonir@live.com>
2021-04-04 21:12:44 +0100

    Add state to application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
+import { useState } from "react";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
 export function App() {
+  const [tweets, setTweets] = useState([]);
+
   return (
     <>
       <h1>Hello World!</h1>
