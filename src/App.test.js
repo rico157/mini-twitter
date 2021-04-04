fe4c6816a55cc5fe67c7e8fc94278985163d352c
Riccardo Cogoni <cogonir@live.com>
2021-04-04 19:29:03 +0100

    Add test - Sing-Out button is rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
 import { render } from "@testing-library/react";
-import AppWithAuthenticator from "./App";
+import AppWithAuthenticator, { App } from "./App";
 
 test("renders Sign-Up page", async () => {
   const { getByText } = render(<AppWithAuthenticator />);
   const signUp = getByText(
     (_, element) => element.tagName.toLowerCase() === "amplify-authenticator"
+  );
+  expect(signUp).toBeTruthy();
+});
+
+test("renders Sign-Out button", async () => {
+  const { getByText } = render(<App />);
+  const signUp = getByText(
+    (_, element) => element.tagName.toLowerCase() === "amplify-sign-out"
   );
   expect(signUp).toBeTruthy();
 });
