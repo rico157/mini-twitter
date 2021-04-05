854a55e4aff22608252cda52270a10092f1bca6a
Riccardo Cogoni <cogonir@live.com>
2021-04-05 23:23:32 +0100

    Add App components tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -23,5 +23,27 @@ describe("App component", () => {
       expect(signOutButton).toBeTruthy();
       expect(signOutButton).toBeInTheDocument();
     });
+
+    test("renders title", () => {
+      const { getByText } = render(<App />);
+      const Title = getByText(/Mini Twitter/);
+      expect(Title).toBeInTheDocument();
+    });
+
+    test("renders a form", () => {
+      const { getByText } = render(<App />);
+      const form = getByText(
+        (_, element) => element.tagName.toLowerCase() === "form"
+      );
+      expect(form).toBeInTheDocument();
+    });
+
+    test("renders a list", () => {
+      const { getByText } = render(<App />);
+      const ul = getByText(
+        (_, element) => element.tagName.toLowerCase() === "ul"
+      );
+      expect(ul).toBeInTheDocument();
+    });
   });
 });
