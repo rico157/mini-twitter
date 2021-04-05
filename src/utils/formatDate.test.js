a9c6a490fc6c43fde635b28593c6130e478dd0a0
Riccardo Cogoni <cogonir@live.com>
2021-04-05 02:21:33 +0100

    Add test - correct formatted date

--- src/utils/formatDate.test.js
+++ src/utils/formatDate.test.js
@@ -3,3 +3,7 @@ import formatDate from "./formatDate";
 test("returns a string", () => {
   expect(typeof formatDate(new Date())).toBe("string");
 });
+
+test("returns the correct formatted date", () => {
+  expect(formatDate(new Date())).toMatch(/\d{2}\/\d{2}\/\d{4}$/);
+});
