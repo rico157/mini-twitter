dc582419ba5320009156fcb0a066d8471bb901a0
Riccardo Cogoni <cogonir@live.com>
2021-04-05 04:58:51 +0100

    Add function to fetch tweets by date

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,26 @@
 import { API, graphqlOperation } from "aws-amplify";
-import { listTweets } from "./queries";
+import { createTweet } from "./mutations";
+import { listTweets, listTweetsByDate } from "./queries";
 
 export const getTweets = async () => {
   const { data } = await API.graphql(graphqlOperation(listTweets));
   return data?.listTweets?.items;
+};
+
+export const getTweetsByDate = async () => {
+  const { data } = await API.graphql(
+    graphqlOperation(listTweetsByDate, {
+      type: "tweet",
+      sortDirection: "DESC"
+    })
+  );
+  return data?.tweetsByDate?.items;
+};
+
+export const insertTweet = async (message) => {
+  const { data } = await API.graphql(
+    graphqlOperation(createTweet, { input: message })
+  );
+  console.log(data);
+  return data?.items;
 };
