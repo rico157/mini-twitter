e79b21eaa5f736501a2ac33665870fadf34b4341
Riccardo Cogoni <cogonir@live.com>
2021-04-05 02:54:06 +0100

    Wrap tests in "describe"

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,25 @@
 import { render } from "@testing-library/react";
 import AppWithAuthenticator, { App } from "./App";
 
-test("renders Sign-Up page", () => {
-  const { getByText } = render(<AppWithAuthenticator />);
-  const signUp = getByText(
-    (_, element) => element.tagName.toLowerCase() === "amplify-authenticator"
-  );
-  expect(signUp).toBeTruthy();
-});
+describe("App component", () => {
+  describe("App with authenticator", () => {
+    test("renders Sign-Up page", () => {
+      const { getByText } = render(<AppWithAuthenticator />);
+      const signUp = getByText(
+        (_, element) =>
+          element.tagName.toLowerCase() === "amplify-authenticator"
+      );
+      expect(signUp).toBeTruthy();
+    });
+  });
 
-test("renders Sign-Out button", () => {
-  const { getByText } = render(<App />);
-  const signUp = getByText(
-    (_, element) => element.tagName.toLowerCase() === "amplify-sign-out"
-  );
-  expect(signUp).toBeTruthy();
+  describe("App without authenticator", () => {
+    test("renders Sign-Out button", () => {
+      const { getByText } = render(<App />);
+      const signUp = getByText(
+        (_, element) => element.tagName.toLowerCase() === "amplify-sign-out"
+      );
+      expect(signUp).toBeTruthy();
+    });
+  });
 });
