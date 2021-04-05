71d73a1f557dee9eafdf1fbcb7ec37644ee42b88
Riccardo Cogoni <cogonir@live.com>
2021-04-05 12:54:37 +0100

    Implement create tweet input form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { useEffect, useState } from "react";
-import { getTweetsByDate } from "./API/api";
+import { getTweetsByDate, insertTweet } from "./API/api";
 import ListTweets from "./Components/ListTweets";
 
 Amplify.configure(awsconfig);
 
 export function App() {
   const [tweets, setTweets] = useState([]);
+  const [inputTweet, setInputTweet] = useState("");
 
   useEffect(() => {
     getTweetsByDate()
@@ -16,9 +17,26 @@ export function App() {
       .catch(console.log);
   }, []);
 
+  const handleChange = (event) => {
+    setInputTweet(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    insertTweet({ message: inputTweet, type: "tweet" })
+      .then(({ createTweet }) => setTweets([createTweet, ...tweets]))
+      .catch(console.log);
+  };
   return (
     <>
       <h1>Hello World!</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          placeholder="What's happening?"
+          value={inputTweet}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
       <ListTweets tweets={tweets} />
       <AmplifySignOut />
     </>
