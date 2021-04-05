e0797712af2c0fa70e4cd7587200823adf4d3bf1
Riccardo Cogoni <cogonir@live.com>
2021-04-05 02:26:51 +0100

    Add test - less than a minute date format

--- src/utils/formatDate.test.js
+++ src/utils/formatDate.test.js
@@ -7,3 +7,9 @@ test("returns a string", () => {
 test("returns the correct formatted date", () => {
   expect(formatDate(new Date())).toEqual("just now");
 });
+
+test("returns the correct formatted date in less than a minute", () => {
+  expect(formatDate(new Date() - 10000)).toEqual("10 seconds ago");
+  expect(formatDate(new Date() - 30000)).toEqual("half a minute ago");
+  expect(formatDate(new Date() - 50000)).toEqual("less than a minute ago");
+});
