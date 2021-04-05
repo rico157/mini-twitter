a27a139ec663a561365c85dd1c97b057c1c1e9cf
Riccardo Cogoni <cogonir@live.com>
2021-04-05 23:27:49 +0100

    Extract and reuse function to get tags

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,13 @@
 import { render } from "@testing-library/react";
 import AppWithAuthenticator, { App } from "./App";
 
+const getTag = (tag) => (_, element) => element.tagName.toLowerCase() === tag;
+
 describe("App component", () => {
   describe("App with authenticator", () => {
     test("renders Sign-Up page", () => {
       const { getByText } = render(<AppWithAuthenticator />);
-      const signUp = getByText(
-        (_, element) =>
-          element.tagName.toLowerCase() === "amplify-authenticator"
-      );
+      const signUp = getByText(getTag("amplify-authenticator"));
       expect(signUp).toBeTruthy();
       expect(signUp).toBeInTheDocument();
     });
@@ -17,9 +16,7 @@ describe("App component", () => {
   describe("App without authenticator", () => {
     test("renders Sign-Out button", () => {
       const { getByText } = render(<App />);
-      const signOutButton = getByText(
-        (_, element) => element.tagName.toLowerCase() === "amplify-sign-out"
-      );
+      const signOutButton = getByText(getTag("amplify-sign-out"));
       expect(signOutButton).toBeTruthy();
       expect(signOutButton).toBeInTheDocument();
     });
@@ -32,17 +29,13 @@ describe("App component", () => {
 
     test("renders a form", () => {
       const { getByText } = render(<App />);
-      const form = getByText(
-        (_, element) => element.tagName.toLowerCase() === "form"
-      );
+      const form = getByText(getTag("form"));
       expect(form).toBeInTheDocument();
     });
 
     test("renders a list", () => {
       const { getByText } = render(<App />);
-      const ul = getByText(
-        (_, element) => element.tagName.toLowerCase() === "ul"
-      );
+      const ul = getByText(getTag("ul"));
       expect(ul).toBeInTheDocument();
     });
   });
