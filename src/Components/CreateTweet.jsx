3f1ea1f58e1f27f07636801f39d9bd30a0f01c39
Riccardo Cogoni <cogonir@live.com>
2021-04-05 19:09:57 +0100

    Add create tweet style

--- src/Components/CreateTweet.jsx
+++ src/Components/CreateTweet.jsx
@@ -1,4 +1,33 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledTextarea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  font-size: 20px;
+  font-family: Helvetica, arial, sans-serif;
+  padding: 30px;
+  border: 1px solid #aaa;
+  box-sizing: border-box;
+`;
+
+const StyledInput = styled.input`
+  width: 77px;
+  height: 40px;
+  border-radius: 9999px;
+  border: none;
+  background-color: #1da1f2;
+  font-weight: 700;
+  color: #fff;
+  margin: 10px 10px 0 0;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
+`;
 
 const CreateTweet = ({ tweets, setTweets, insertTweet }) => {
   const [inputTweet, setInputTweet] = useState("");
@@ -14,14 +43,14 @@ const CreateTweet = ({ tweets, setTweets, insertTweet }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <textarea
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledTextarea
         placeholder="What's happening?"
         value={inputTweet}
         onChange={handleChange}
       />
-      <input type="submit" value="Submit" />
-    </form>
+      <StyledInput type="submit" value="Tweet" />
+    </StyledForm>
   );
 };
 
